2eebacf6e2d5e44f9bf3f64075677a43188579b5
Maria Saldina <sald1na.maria@yandex.ru>
2026-03-24 15:21:28 +0400

    feature(deal): initial commit

---
+++ deal/src/test/java/com/creditscoring/deal/DealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditscoring.deal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
